5e05f70add1125ae4256cf85f3a7a3f32a113b70
orangeoceans <90552466+orangeoceans@users.noreply.github.com>
2022-07-01 14:45:49 -0700

    Add some comments

--- gonddr.js
+++ gonddr.js
@@ -43,15 +43,20 @@ class GonDDR extends Phaser.Scene {
 	{
 		super('game');
 
-		this.arrows;
-		this.song_script;
+		this.arrows;      // Currently active arrows
+		this.song_script; // Hash of strings; maps a tick to a string encoding the arrow(s) to create.
 		this.gondola;
 		this.combo;
 		this.score;
 
-		this.tps = 100;
+		this.tps = 100; // Ticks per second. 
+						// A tick is the smallest time step in the song script, NOT a frame or Phaser loop.
+						// Speed, position, and timing of arrows are measured in ticks.
+						// Adjust this change the speed of the song.
 		this.fall_ticks = 400;
-		this.hit_window 
+						// # of ticks for a standard arrow to fall from the top to bottom of the screen.
+		this.hit_window;
+						// Margin in ticks that a player's button press registers as a hit
 		this.clock;
 
 		//TODO
@@ -82,7 +87,7 @@ class GonDDR extends Phaser.Scene {
 
 	read_song_file: function()
 	{
-
+		//TODO
 	},
 
 	update_arrows: function(this_tick)
