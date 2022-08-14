3771a3eaee2e61b8e53c43c397fe193e49463f4c
superluumic <mathew@superlumic.io>
2022-08-13 20:31:49 -0400

    Finish beatmap

--- js/scenes/gonddr.js
+++ js/scenes/gonddr.js
@@ -155,6 +155,7 @@ class GonDDR extends Phaser.Scene {
 	// Load song data, set BPM and time signature
 	init_song() {
 		this.song = this.cache.json.get('testdance');
+		console.log(this.song);
 		this.arrow_idx = 0;
 		this.action_idx = 0;
 
